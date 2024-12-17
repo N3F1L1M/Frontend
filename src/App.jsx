f7857799e7d6030dc572585722fcbb66c1ddcbcd
N3F1L1M <cordovaluis0311@gmail.com>
2024-12-17 04:11:51 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Barradebusqueda } from "./components/Barradebusqueda";
+import { About, Contact, Home, Services } from "./components/pages";
+
+function App() {
+  return (
+    <div className="App">
+
+      
+      <Navbar />
+      <Barradebusqueda/>
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Barradebusqueda.jsx
@@ -0,0 +1,26 @@
+import { React, useState } from "react";
+import './Barradebusqueda.css';
+
+
+
+
+export const Barradebusqueda = () => {
+
+  
+
+  return (
+    <div className="container">
+      
+      <input
+        type="text"
+        placeholder="Type your query here"
+
+      />
+
+      <botton className="boton">hola</botton>
+    </div>
+  );
+};
+
+
+{/*  */};
