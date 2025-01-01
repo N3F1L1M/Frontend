19be98632b6ed90a207eeaf09b7943ec41bc49dd
N3F1L1M <cordovaluis0311@gmail.com>
2025-01-01 00:33:08 -0500

    css barrabusqueda 1.2

--- src/components/Barradebusqueda.jsx
+++ src/components/Barradebusqueda.jsx
@@ -4,14 +4,23 @@ import "./css/Barradebusqueda.css";
 
 
 const Barradebusqueda = () => {
+
+
+    const Botonbuscar = () => {
+
+        console.log('Hola mundo');
+    }
+
+
     return (
       <div className="search-bar-container">
         <input
+           
           type="text"
           className="search-bar-input"
           placeholder="Buscar..."
         />
-        <button className="search-bar-button">Buscar</button>
+        <button onClick={Botonbuscar} className="search-bar-button" >Buscar</button>
       </div>
     );
   };
