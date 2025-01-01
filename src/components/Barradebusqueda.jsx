aafb7969385f289f4a991170dd713a186017fbc1
N3F1L1M <cordovaluis0311@gmail.com>
2025-01-01 00:14:09 -0500

    css de la barra de busqueda con chat gpt

--- src/components/Barradebusqueda.jsx
+++ src/components/Barradebusqueda.jsx
@@ -3,14 +3,17 @@ import "./css/Barradebusqueda.css";
 
 
 
-
-
 const Barradebusqueda = () => {
-  return <div>hola como estas</div>;
-
-
-};
-
-
-
-export default Barradebusqueda;
+    return (
+      <div className="search-bar-container">
+        <input
+          type="text"
+          className="search-bar-input"
+          placeholder="Buscar..."
+        />
+        <button className="search-bar-button">Buscar</button>
+      </div>
+    );
+  };
+  
+  export default Barradebusqueda;
