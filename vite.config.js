1f8c93a3febf5286824757489611bdfb0aa59bca
N3F1L1M <cordovaluis0311@gmail.com>
2025-02-12 21:13:41 -0500

    cambios en vite,config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Usa el puerto que Render asigna o 3000 por defecto
+    host:'0.0.0.0', // Necesario para que Render pueda acceder
+  },
+});
