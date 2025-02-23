f218914daab452f2d92c2f4b593944c69ba2a548
N3F1L1M <cordovaluis0311@gmail.com>
2025-02-23 11:22:49 -0500

    componente contenedor de producto

---
+++ src/components/contenedordeproducto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./css/contenedordeproducto.css";
+
+const contenedordeproducto = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default contenedordeproducto
